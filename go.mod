bf8c4b34bb50329cecbd6f5f7bb67b779bad19b6
Roman Nozhenko <oatsmoke@bk.ru>
2024-08-05 16:57:46 +0600

    add uuid

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/oatsmoke/hello_world
 
 go 1.22.5
+
+require github.com/gofrs/uuid v4.4.0+incompatible
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gofrs/uuid"
+)
+
 func main() {
-	println("Hello, World!")
+	uid, _ := uuid.NewV4()
+	fmt.Println(uid.String())
 }
